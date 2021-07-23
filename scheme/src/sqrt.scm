0c0aefaf7f85806f688d01c9d3dba0403e01a9d8
jaymzee <jaymzee@users.noreply.github.com>
2021-07-23 11:38:52 -0400

    scheme sqrt

---
+++ scheme/src/sqrt.scm
@@ -0,0 +1,23 @@
+(define tolerance 0.000001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (my-sqrt x)
+  (fixed-point
+    (average-damp (lambda (y) (/ x y)))
+    1.0))
